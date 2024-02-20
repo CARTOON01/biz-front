954697d8f1b540ccb0c862f71193b02fcbe89be4
CARTOON <brianngugi224@gmail.com>
2024-02-20 04:59:35 +0300

    adding button components & improvement on create store

--- components/modals/store-model.tsx
+++ components/modals/store-model.tsx
@@ -1,17 +1,65 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Modal } from "@/components/ui/modal";
 import { usestoreModalStore } from "@/hooks/use-store-modal";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
 export const StoreModal = () => {
     const StoreModal = usestoreModalStore();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+    //Store creation
+}
+
+
     return (
     <Modal title="Create store"
     description="Add a new store to manage products and categories"
     isOpen={StoreModal.isOpen}
     onClose={StoreModal.onClose}
     >
-     Future Create from store  
+    <div>
+        <div className="space-y-4 py-2 pb-4">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField 
+                        control={form.control}
+                        name="name"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Biz-Net" {...field}/>
+                                </FormControl>
+                            </FormItem>
+                        )}
+                        />
+                        <div className="pt-4 space-x-2 flex items-center justify-end w-full">
+                            <Button>Cancel</Button>
+                            <Button>Continue</Button>
+
+                        </div>
+                </form>
+            </Form>
+        </div>
+     </div>
     </Modal>
     );
 };
