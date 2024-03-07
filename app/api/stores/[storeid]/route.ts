3f5d40be2cb7bceb88636aad7ab679982eeb83cc
CARTOON <brianngugi224@gmail.com>
2024-03-07 13:16:29 +0300

    adding delete function

--- app/api/stores/[storeid]/route.ts
+++ app/api/stores/[storeid]/route.ts
@@ -40,3 +40,34 @@ export async function PATCH (
         return new NextResponse('Internal Error', { status: 500 });
     }
 }
+export async function DELETE (
+    req: Request,
+    { params }: { params: { storeid: string}}
+) {
+    try {
+        const { userId } = auth();
+
+
+
+        if (!userId) {
+        return new NextResponse('Unauthenticated', { status: 401 });
+    } 
+        if (!params.storeid) {
+            return new NextResponse('Store ID is Required', { status: 400 });
+        }
+
+    
+        const store = await prismadb.store.deleteMany({
+            where:{
+                id: params.storeid,
+                userId
+            }
+        });
+
+        return NextResponse.json(store);
+    }
+    catch (error) {
+        console.log('[STORE_DELETE]', error);
+        return new NextResponse('Internal Error', { status: 500 });
+    }
+}
